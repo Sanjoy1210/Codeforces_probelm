e8da54f4c254d4fc876dbd3b215335d89d5b46cc
Sanjoy1210 <62760864+Sanjoy1210@users.noreply.github.com>
2020-08-30 16:30:23 +0600

    Add files via upload

---
+++ new/codeforces_469A.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    int n, p, a;
+    scanf("%d", &n);
+    bool ara[105];
+
+//    for(int i = 0; i <= n; i++){
+//        ara[i] = 0;
+//    }
+    scanf("%d", &p);
+    for(int i = 1; i <= p; i++){
+        scanf("%d", &a);
+        ara[a] = 1;
+    }
+    scanf("%d", &p);
+    for(int i = 1; i <= p; i++){
+        scanf("%d", &a);
+        ara[a] = 1;
+    }
+
+    for(int i = 1; i <= n; i++){
+        if(ara[i] == 0){
+            printf("Oh, my keyboard!\n");
+            return 0;
+        }
+    }
+
+    printf("I become the guy.\n");
+
+    return 0;
+}
